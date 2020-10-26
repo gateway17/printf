102f7810d7156bf6d03aa6e3458959dc74a9e5fc
gateway17 <gateway17ssh@gmail.com>
2020-10-26 21:39:39 +0000

    Working on.

---
+++ printf.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+int _printf(const char *format, ...)
+{
+int i = 0;
+
+va_list arg;
+/*estructura*/
+/**
+pt_t types[] = {
+{"i", "int", print_int}, 
+{NULL, NULL}
+};
+
+/*va_start*/
+/**
+va_start(arg, format);
+if (/* condicionales, tenía ejemplos, pero acá van?*/)
+/**
+return (-1);
+*/
+
+int _printf(const char *format, ...)
+{
+int i = 0;
+for (;format[i] != '\0';i++)
+{ /* otro while?*/
+
+	write(1,format[i],1);
+
+} 
+}
