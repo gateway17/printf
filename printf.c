243b4e411b03e0695863ab1ff685dac73df9e2ae
gateway17 <gateway17ssh@gmail.com>
2020-10-27 00:34:37 +0000

    Working on.

--- printf.c
+++ printf.c
@@ -5,29 +5,50 @@ int _printf(const char *format, ...)
 {
 int i = 0;
 
-va_list arg;
-/*estructura*/
-/**
-pt_t types[] = {
-{"i", "int", print_int}, 
-{NULL, NULL}
-};
-
 /*va_start*/
 /**
 va_start(arg, format);
-if (/* condicionales, tenía ejemplos, pero acá van?*/)
+if ( condicionales, tenía ejemplos, pero acá van?)
 /**
 return (-1);
 */
+int _printf(const char *format, ...);
+
+int main (void)
+{
+	char *p = "Hello my name is %c";
+
+	_printf(p);
+	return(0);
+}
 
 int _printf(const char *format, ...)
 {
 int i = 0;
 for (;format[i] != '\0';i++)
-{ /* otro while?*/
+{
+	if (format[i] == '%')
+	{
+		i++;
+		switch (format[i])
+		{
+			case 'c':
+				printf("Andres, Si funciona caracter.");
+				break;
+			case 'd':
+				printf("Si funciona numero entero");
+				break;
+		}
 
-	write(1,format[i],1);
+	}
+	else
+	{
 
+	write(1,&format[i],1);
+
+	}
 } 
 }
+
+
+
