2dfaae7393074224dff247ffbf7e60ce48ea5cf0
helectron <helena.cortes@udea.edu.co>
2020-10-28 00:07:10 +0000

    fixing style

--- funciones.c
+++ funciones.c
@@ -1,44 +1,66 @@
 #include "holberton.h"
 
+/**
+ * _reverse - reverse string
+ * @s: array
+ * Return: none
+ */
 void _reverse(char s[])
- {
-     int i, j;
-     char c;
- 
-     for (i = 0, j = _strl(s)-1; i<j; i++, j--) {
-         c = s[i];
-         s[i] = s[j];
-         s[j] = c;
-     }
- }
-
-void _itoa(int n, char s[])
- {
-     int i, sign;
- 
-     if ((sign = n) < 0)  /* record sign */
-         n = -n;          /* make n positive */
-     i = 0;
-     do {       /* generate digits in reverse order */
-         s[i++] = n % 10 + '0';   /* get next digit */
-     } while ((n /= 10) > 0);     /* delete it */
-     if (sign < 0)
-         s[i++] = '-';
-     s[i] = '\0';
-     _reverse(s);
- }
-
- int _putchar(char c)
 {
-	return(write(1, &c, 1));
+	int i, j;
+	char c;
+
+	for (i = 0, j = _strl(s) - 1; i < j; i++, j--)
+	{
+		c = s[i];
+		s[i] = s[j];
+		s[j] = c;
+	}
 }
 
-/* string length*/
+/**
+ * _itoa - convert int to char
+ * @s: become to char
+ * @n: integer to be converted
+ * Return: none
+ */
+void _itoa(int n, char s[])
+{
+	int i;
+	int sign = n;
+
+	if (sign < 0)  /* record sign */
+	n = -n;          /* make n positive */
+	i = 0;
+	do {       /* generate digits in reverse order */
+	s[i++] = n % 10 + '0';   /* get next digit */
+	} while ((n /= 10) > 0);     /* delete it */
+	if (sign < 0)
+	s[i++] = '-';
+	s[i] = '\0';
+	_reverse(s);
+}
+
+/**
+ * _putchar - print char to char
+ * @c: char
+ * Return: none
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * _strl - string length
+ * @str: pointer to a string
+ * Return: string
+ */
 int _strl(char *str)
 {
 	int i = 0;
+
 	while (str[i] != '\0')
 		i++;
-	return(i);
+	return (i);
 }
-	
